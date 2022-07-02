60ecf82903c31111fd1f79e9ba6b3b84fc054516
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-07-02 20:27:17 +0700

    finish building app

--- app/src/App.js
+++ app/src/App.js
@@ -5,22 +5,22 @@ import {
   useMoralisSolanaCall,
 } from "react-moralis";
 import {
+  Badge,
   Button,
   Row,
   Select,
   Icon,
   Blockie,
-  CopyButton,
-  useNotification,
+  Tag,
   getEllipsisTxt,
 } from "web3uikit";
 import SolanaNFTCard from "./components/SolanaNFTCard";
+import MoralisLogo from "./assets/moralis_logo.jpeg";
 
 const { Col } = Row;
 
 const App = () => {
   const [chain, setChain] = useState("devnet");
-  const notify = useNotification();
   const SolanaApi = useMoralisSolanaApi();
   const {
     user,
@@ -31,9 +31,10 @@ const App = () => {
     isAuthenticating,
     authenticate,
     enableWeb3,
+    logout,
   } = useMoralis();
 
-  const { data, error, isLoading } = useMoralisSolanaCall(
+  const { data } = useMoralisSolanaCall(
     SolanaApi.account.getNFTs,
     {
       network: chain,
@@ -63,80 +64,102 @@ const App = () => {
 
   return (
     <Row alignItems="center" justifyItems="center">
-      <Col isFullWidth>
-        <div style={{ margin: "1rem" }}>
-          <Row alignItems="center" justifyItems="end">
-            <Select
-              defaultOptionIndex={1}
-              width="220px"
-              value={chain}
-              onChange={(option) => setChain(option?.id)}
-              options={[
-                {
-                  id: "mainnet",
-                  label: "Solana Mainnet",
-                  prefix: <Icon fill="#68738D" svg="solana" />,
-                },
-                {
-                  id: "devnet",
-                  label: "Solana Devnet",
-                  prefix: <Icon fill="#68738D" svg="solana" />,
-                },
-              ]}
-            />
-            {isAuthenticated ? (
-              <div
-                style={{
-                  display: "flex",
-                  padding: "10px",
-                  border: "1px solid",
-                  borderRadius: "20px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: "5px",
-                }}
-              >
-                <Blockie seed={user?.get("solAddress")} />
-                {getEllipsisTxt(user?.get("solAddress"))}
-                <CopyButton
-                  text={`${getEllipsisTxt(user?.get("solAddress"))}`}
-                  revertIn={6500}
-                  onCopy={() =>
-                    notify({
-                      type: "success",
-                      message: "Copied to clipboard",
-                      title: "New Notification",
-                      position: "topR",
-                    })
-                  }
-                />
-              </div>
-            ) : (
-              <Button
-                icon="solana"
-                onClick={() => authenticate({ type: "sol" })}
-                isLoading={isAuthenticating || isWeb3EnableLoading}
-                size="large"
-                text="Connect Wallet"
-                theme="primary"
-                type="button"
-                style={{ height: "56px" }}
+      <Col
+        isFullWidth
+        style={{
+          boxShadow: "rgb(0 0 0 / 8%) 0px 4px 15px",
+          zIndex: 100,
+        }}
+      >
+        <div style={{ margin: "0.5rem" }}>
+          <Row alignItems="center" justifyItems="space-between">
+            <img src={MoralisLogo} alt="Moralis" width="80px" height="auto" />
+            <Row alignItems="center">
+              <Select
+                defaultOptionIndex={1}
+                width="220px"
+                value={chain}
+                onChange={(option) => setChain(option?.id)}
+                options={[
+                  {
+                    id: "mainnet",
+                    label: "Solana Mainnet",
+                    prefix: <Icon fill="#68738D" svg="solana" />,
+                  },
+                  {
+                    id: "devnet",
+                    label: "Solana Devnet",
+                    prefix: <Icon fill="#68738D" svg="solana" />,
+                  },
+                ]}
               />
-            )}
+              {isAuthenticated ? (
+                <div onClick={() => logout()} style={{ cursor: "pointer" }}>
+                  <Blockie seed={user?.get("solAddress")} size={12} />
+                </div>
+              ) : (
+                <Button
+                  icon="solana"
+                  onClick={() => authenticate({ type: "sol" })}
+                  isLoading={isAuthenticating || isWeb3EnableLoading}
+                  size="large"
+                  text="Connect Wallet"
+                  theme="primary"
+                  type="button"
+                  style={{ height: "56px" }}
+                />
+              )}
+            </Row>
           </Row>
         </div>
       </Col>
-      <Col isFullWidth>
-        <Row alignItems="center" justifyItems="center">
-          <h1>Solana NFT Portfolio</h1>
-        </Row>
-      </Col>
-      <Col isFullWidth>
-        {JSON.stringify(data)}
-        {data?.map((nft) => {
-          return <SolanaNFTCard chain={chain} nftAddress={nft?.mint} />;
-        })}
-      </Col>
+      {isAuthenticated ? (
+        <>
+          <Col isFullWidth style={{ marginTop: "3rem" }}>
+            <Row alignItems="center" justifyItems="center">
+              <Blockie seed={user?.get("solAddress")} size={35} />
+            </Row>
+          </Col>
+          <Col isFullWidth>
+            <Row alignItems="center" justifyItems="center">
+              <Tag text={getEllipsisTxt(user?.get("solAddress"))} />
+            </Row>
+          </Col>
+          <Col isFullWidth style={{ marginLeft: "3rem", marginRight: "3rem" }}>
+            <Badge
+              text="NFT Portfolio"
+              textVariant="h6"
+              style={{ width: "150px", cursor: "pointer" }}
+            />
+          </Col>
+          <Col isFullWidth style={{ marginLeft: "3rem", marginRight: "3rem" }}>
+            <hr style={{ borderTop: "1px solid rgb(0 0 0 / 8%)" }} />
+          </Col>
+          <Col isFullWidth style={{ margin: "3rem" }}>
+            {data?.map((nft) => {
+              return <SolanaNFTCard chain={chain} nftAddress={nft?.mint} />;
+            })}
+          </Col>
+        </>
+      ) : (
+        <>
+          <Col isFullWidth style={{ marginTop: "7rem" }}>
+            <Row alignItems="center" justifyItems="center">
+              <p style={{ textAlign: "center", fontSize: "64px" }}>
+                <b>Solana NFT Dashboard</b>
+              </p>
+            </Row>
+            <Row alignItems="center" justifyItems="center">
+              <img
+                src={MoralisLogo}
+                alt="Moralis"
+                width="300px"
+                height="auto"
+              />
+            </Row>
+          </Col>
+        </>
+      )}
     </Row>
   );
 };
--- app/src/components/SolanaNFTCard.js
+++ app/src/components/SolanaNFTCard.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { Card } from "antd";
 import { useMoralisSolanaApi, useMoralisSolanaCall } from "react-moralis";
-import { Tag, getEllipsisTxt } from "web3uikit";
+import { Row, Tag, Skeleton, getEllipsisTxt } from "web3uikit";
 
 const SolanaNFTCard = (props) => {
   const { chain = "mainnet", nftAddress } = props;
   const [metadata, setMetadata] = useState({});
   const SolanaApi = useMoralisSolanaApi();
-  const { data, error, isLoading } = useMoralisSolanaCall(
+  const { data, isFetching } = useMoralisSolanaCall(
     SolanaApi?.nft?.getNFTMetadata,
     {
       network: chain,
@@ -33,18 +33,38 @@ const SolanaNFTCard = (props) => {
   });
 
   return (
-    <>
-      {JSON.stringify(metadata)}
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={<img alt={metadata?.name} src={metadata?.image} />}
-      >
-        <Card.Meta title={getEllipsisTxt(nftAddress)} />
-        {/* {metadata?.attributes?} */}
-        <Tag text="tex" />
-      </Card>
-    </>
+    <Card
+      hoverable
+      style={{
+        width: 300,
+        boxShadow: "rgb(0 0 0 / 8%) 0px 4px 15px",
+        borderRadius: "15px",
+      }}
+      cover={
+        !isFetching ? (
+          <img alt={metadata?.name} src={metadata?.image} />
+        ) : (
+          <Skeleton theme="image" width="300px" height="300px" />
+        )
+      }
+    >
+      {!isFetching ? (
+        <>
+          <Card.Meta title={getEllipsisTxt(nftAddress)} />
+          <Row alignItems="center" style={{ marginTop: "1rem", marginLeft: 0 }}>
+            {metadata?.attributes?.map((m) => {
+              return <Tag text={m?.value} />;
+            })}
+          </Row>
+        </>
+      ) : (
+        <>
+          <Skeleton theme="text" />
+          <Skeleton theme="text" />
+          <Skeleton theme="text" />
+        </>
+      )}
+    </Card>
   );
 };
 
